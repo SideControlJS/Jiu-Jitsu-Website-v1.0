6d7df74c26f4cf7f5a6d4a64a2f81af5a1fa3307
Jesse <jesse.lare@gmail.com>
2024-04-06 16:19:41 -0400

    adding mongoDB connection

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch(err => console.log(err));
