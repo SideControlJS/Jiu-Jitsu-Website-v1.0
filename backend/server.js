e92785e6995caf67c6b69ac630185ed00d0ec628
Jesse <jesse.lare@gmail.com>
2024-04-13 00:25:14 -0400

    adding calendar and merchandise api routes

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/api/calendar', require('./routes/calendar'));
+app.use('/api/merchandise', require('./routes/merchandise'));
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
