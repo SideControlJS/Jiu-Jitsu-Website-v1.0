a3374bec36e415707d1d112f90de3d4486f064cb
Jesse <jesse.lare@gmail.com>
2024-10-05 17:42:48 -0400

    updating mongouri info and setting up database and models

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const routes = require('./routes'); // Import the centralized routes
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -18,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Use centralized routes
 app.use('/api', routes);
